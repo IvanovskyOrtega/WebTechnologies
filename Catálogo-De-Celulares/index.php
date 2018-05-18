30632493963174711c11ca7883ce8dd26c28234e
IvanovskyOrtega <ivanovskyortega@gmail.com>
2018-05-18 13:13:08 -0500

    Se empieza el catálogo de celulares

---
+++ Catálogo-De-Celulares/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cat&aacute;logo de Celulares</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="./js/index.js"></script>
+  </head>
+  <body>
+    <section class="header">
+      <nav>
+        <div class="nav-wrapper grey darken-3">
+          <a href="#" class="brand-logo center">Cat&aacute;logo de Celulares</a>
+
+        </div>
+      </nav>
+    </section>
+    <section class="content">
+      <div class="container">
+        <div class="row">
+
+        </div>
+      </div>
+    </section>
+    <section class="footer">
+      <footer class="page-footer lime darken-1">
+        <div class="footer-copyright grey darken-3">
+          <div class="container center">
+          © 2018 Tecnolog&iacute;as para la Web con To&ntilde;ito :v
+          </div>
+        </div>
+      </footer>
+    </section>
+  </body>
+</html>
