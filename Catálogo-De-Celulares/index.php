22c0b877e9f8686e5e9f5c5e93d72b488cfc112d
IvanovskyOrtega <ivanovskyortega@gmail.com>
2018-05-18 13:39:03 -0500

    Ejemplo con Image Card

--- Catálogo-De-Celulares/index.php
+++ Catálogo-De-Celulares/index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <title>Cat&aacute;logo de Celulares</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="./js/index.js"></script>
@@ -13,7 +14,7 @@
     <section class="header">
       <nav>
         <div class="nav-wrapper grey darken-3">
-          <a href="#" class="brand-logo center">Cat&aacute;logo de Celulares</a>
+          <a href="#" class="brand-logo center"><i class="fas fa-mobile-alt"></i>Cat&aacute;logo de Celulares</a>
 
         </div>
       </nav>
@@ -21,7 +22,24 @@
     <section class="content">
       <div class="container">
         <div class="row">
-
+          <div class="col s12 m6">
+            <div class="row">
+              <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="media/huaweiMate10.jpg">
+                    <span class="card-title">Huawei Mate 10</span>
+                  </div>
+                  <div class="card-content">
+                    <p>Ejemplo para ver que pedo.</p>
+                  </div>
+                  <div class="card-action">
+                    <a href="#">Ver m&aacute;s</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
