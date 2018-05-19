aabb47173f21d5fe4f8e8a9bcdfa951a9d90b5e2
IvanovskyOrtega <ivanovskyortega@gmail.com>
2018-05-19 13:14:54 -0500

    Se agregan los Modals y Sliders

--- Catálogo-De-Celulares/js/index.js
+++ Catálogo-De-Celulares/js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   var jsons;
 
+
   $.ajax({
 
       url:"./phps/obtenerDispositivos.php",
@@ -10,28 +11,42 @@ $(document).ready(function(){
 
         jsons = jQuery.parseJSON(respAX);
         var dispCard = "";
+        var dispModal = "";
 
         for(var dispositivo in jsons){
           datos = jQuery.parseJSON(jsons[dispositivo]);
-          dispCard ="";
-          dispCard+="<div class='col s12 m6 l4'>";
+          dispCard = "";
+          dispModal = "";
+
+          /* En esta parte agregamos la Materialize Card de cada dispositivo */
+          dispCard+="<div class='"+dispositivo+" col s12 m6 l4'>";
           dispCard+="<div class='card horizontal hoverable'><div class='card-image'>";
           dispCard+="<img src="+datos.imgs[0]+"></div>";
           dispCard+="<div class='card-stacked'><div class='card-content'><p>"+datos.nombre+"</p></div>";
-          dispCard+="<div class='card-action'><button data-target='modal1' class='waves-effect indigo btn modal-trigger' id="+dispositivo+">Ver</button></div>";
+          dispCard+="<div class='card-action'><button data-target='"+dispositivo+"modal' class='waves-effect indigo btn modal-trigger' id="+dispositivo+">Ver</button></div>";
           dispCard+="</div></div></div>";
-
           $(".dispositivos").append(dispCard);
+
+          /* En esta parte agregamos el Modal de cada dispositivo */
+          dispModal +="<div id='"+dispositivo+"modal' class='modal'>";
+          dispModal +="<div class='modal-content'><h4>"+datos.nombre+"</h4>";
+          dispModal +="<div class='row'><div class='col s6'><div class='slider'><ul class='slides'><li><img src="+datos.imgs[0]+"></li><li><img src="+datos.imgs[1]+"></li><li><img src="+datos.imgs[2]+"></li></ul></div></div><div class='col s6'></div></div></div><div class='modal-footer'>";
+          dispModal +="<a href='#!' class='modal-close waves-effect waves-green btn-flat'>Cerrar</a>";
+          dispModal +="</div></div>";
+          $(".modals").append(dispModal);
+
         }
+
+        /* Se inicializan despues de haber sido agregados */
+        $('.modal').modal();
+        $('.slider').slider();
 
       }
   });
 
+
   $(".dispositivos").on("click",".btn",function(){
       var dispositivo = $(this).attr("id");
-      //alert(jsons[dispositivo]);
   });
-
-  $('.modal').modal();
 
 });
