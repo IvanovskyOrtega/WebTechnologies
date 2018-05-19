5c7113aa5896dcb07dc9cabaabf65eff4e9b8526
Ivan Ortega Victoriano <ivanovskyortega@gmail.com>
2018-05-19 00:14:04 -0500

    Comentario en la conexión con la base de datos.

--- Catálogo-De-Celulares/phps/obtenerDispositivos.php
+++ Catálogo-De-Celulares/phps/obtenerDispositivos.php
@@ -1,5 +1,6 @@
 <?php
   require("conexionBD.php");
+  // Aqui cambien dependiendo como tengan su usuario y pass :v
   $conector = new Conector("root","root","localhost","jsons");
   $conector->conectar();
   $conector->query("select * from dispositivos");
