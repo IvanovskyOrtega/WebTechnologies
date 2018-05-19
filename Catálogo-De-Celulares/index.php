6d410315417196f056b8c5a629f6668f63ee9486
IvanovskyOrtega <ivanovskyortega@gmail.com>
2018-05-19 17:17:02 -0500

    Alabado sea Fotorama :,v

--- Catálogo-De-Celulares/index.php
+++ Catálogo-De-Celulares/index.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo|Quicksand|Raleway">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
     <script src="./js/index.js"></script>
@@ -39,8 +41,7 @@
 
           </div>
         </div>
-
-      </div>
+        
     </section>
     <section class="footer">
       <footer class="page-footer red darken-3">
--- Catálogo-De-Celulares/js/index.js
+++ Catálogo-De-Celulares/js/index.js
@@ -19,27 +19,33 @@ $(document).ready(function(){
           dispModal = "";
 
           /* En esta parte agregamos la Materialize Card de cada dispositivo */
-          dispCard+="<div class='"+dispositivo+" col s12 m6 l4'>";
-          dispCard+="<div class='card horizontal hoverable'><div class='card-image'>";
-          dispCard+="<img src="+datos.imgs[0]+"></div>";
-          dispCard+="<div class='card-stacked'><div class='card-content'><p>"+datos.nombre+"</p></div>";
-          dispCard+="<div class='card-action'><button data-target='"+dispositivo+"modal' class='waves-effect indigo btn modal-trigger' id="+dispositivo+">Ver</button></div>";
-          dispCard+="</div></div></div>";
+          dispCard+= "<div class='"+dispositivo+" col s12 m6 l4'>";
+          dispCard+= "<div class='card horizontal hoverable'><div class='card-image'>";
+          dispCard+= "<img src="+datos.imgs[0]+"></div>";
+          dispCard+= "<div class='card-stacked'><div class='card-content'><p>"+datos.nombre+"</p></div>";
+          dispCard+= "<div class='card-action'><button data-target='"+dispositivo+"modal' class='waves-effect indigo btn modal-trigger' id="+dispositivo+">Ver</button></div>";
+          dispCard+= "</div></div></div>";
           $(".dispositivos").append(dispCard);
 
           /* En esta parte agregamos el Modal de cada dispositivo */
-          dispModal +="<div id='"+dispositivo+"modal' class='modal'>";
-          dispModal +="<div class='modal-content'><h4>"+datos.nombre+"</h4>";
-          dispModal +="<div class='row'><div class='col s6'><div class='slider'><ul class='slides'><li><img src="+datos.imgs[0]+"></li><li><img src="+datos.imgs[1]+"></li><li><img src="+datos.imgs[2]+"></li></ul></div></div><div class='col s6'></div></div></div><div class='modal-footer'>";
-          dispModal +="<a href='#!' class='modal-close waves-effect waves-green btn-flat'>Cerrar</a>";
-          dispModal +="</div></div>";
+          dispModal += "<div id='"+dispositivo+"modal' class='modal modal-fixed-footer'>";
+          dispModal += "<div class='modal-content'><h4 class='center-align'>"+datos.nombre+"</h4>";
+          dispModal += "<div class='col s12 m4'>";
+          dispModal += "<div class='fotorama' data-transition='dissolve' data-nav='thumbs'><img src="+datos.imgs[0]+">";
+          dispModal += "<img src="+datos.imgs[1]+">";
+          dispModal += "<img src="+datos.imgs[2]+"></div></div>";
+          dispModal += "<div class='col s12 m6'></div></div>";
+          dispModal += "<div class='modal-footer'>";
+          dispModal += "<a href='#!' class='modal-close waves-effect waves-green btn-flat'>Cerrar</a>";
+          dispModal += "</div></div>";
           $(".modals").append(dispModal);
 
         }
 
         /* Se inicializan despues de haber sido agregados */
         $('.modal').modal();
-        $('.slider').slider();
+        $('.slider').slider({fullWidth: false});
+        $('.fotorama').fotorama();
 
       }
   });
