913b76ee9ac1f756df7669c045116ce0df5e74c8
Ivan Ortega Victoriano <ivanovskyortega@gmail.com>
2018-05-30 03:27:28 -0500

    Ya no se necesita la conexión a base de datos.

--- Catálogo-De-Celulares/phps/obtenerDispositivos.php
+++ Catálogo-De-Celulares/phps/obtenerDispositivos.php
@@ -1,7 +1,5 @@
 <?php
-  require("conexionBD.php");
-  // Aqui cambien dependiendo como tengan su usuario y pass :v
-  $conector = new Conector("root","root","localhost","jsons");
-  $conector->conectar();
-  $conector->query("SELECT *, JSON_EXTRACT(d, '$.nombre') AS nombre FROM dispositivos ORDER BY nombre ASC;");
+  /* Ya no hay necesidad de base de datos */
+  $dispositivos = file_get_contents("./../dispositivos.json");
+  echo $dispositivos;
 ?>
