e777f213b9cfd7376221faeea41c83bd135e4dc6
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-06-03 07:28:01 -0500

    Portada

--- Catálogo-De-Celulares/index.php
+++ Catálogo-De-Celulares/index.php
@@ -4,59 +4,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat&aacute;logo de Celulares</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css"> 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo|Quicksand|Raleway">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/index.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script src="./js/index.js"></script>
-    <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+
   </head>
   <body>
-
-    <section class="header">
-      <header>
-
-          <nav class="nav-extended blue-grey darken-4">
-            <div class="nav-content center">
-              <span class="nav-title "><i class="fas fa-mobile-alt fa-1x lime-text"></i> Cat&aacute;logo de celulares</span>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
             </div>
-          </nav>
-
-      </header>
-    </section>
-
-    <section class="content">
-      <div id="disp-container" class="container">
-
-        <div class="row dispositivos">
-          <div class="dispositivos">
-
           </div>
-        </div>
-
-        <div class="row">
-          <div class="modals">
-
+          <div class="inner cover">
+            <h1 class="cover-heading">Cat&aacute;logo de Celulares</h1>
+            <p class="lead">
+              Aqu&iacute; encontrar&aacute;s los dispositivos m&aacute;s sobresalientes
+              del mercado y un listado de sus caracter&iacute;sticas.
+            </p>
+            <p class="lead">
+              <a type="button" href="./catalogo.html" class="btn btn-lg btn-secondary">Ir a la b&uacute;squeda</a>
+            </p>
           </div>
         </div>
       </div>
-    </section>
-
-    <section class="footer">
-      <footer class="page-footer lime">
-        <div class="footer-copyright blue-grey darken-4">
-          <div class="container center">
-          &copy; 2018 Tecnolog&iacute;as para la Web ESCOM
-          </div>
-        </div>
-      </footer>
-    </section>
-
+    </div>
   </body>
 </html>
